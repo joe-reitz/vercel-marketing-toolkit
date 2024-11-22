5c69406320fe0251953adb49a9e737f9f3d9fb89
Joe Reitz <joe.reitz@vercel.com>
2024-11-22 15:25:28 -0800

    eslint file

--- src/components/ui/use-toast.ts
+++ src/components/ui/use-toast.ts
@@ -22,7 +22,7 @@ const actionTypes = {
   REMOVE_TOAST: "REMOVE_TOAST",
 } as const
 
-type ActionTypes = typeof actionTypes[keyof typeof actionTypes]
+type ActionType = typeof actionTypes[keyof typeof actionTypes]
 
 type Action =
   | {
